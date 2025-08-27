5ce0f11fbedc64f78a316f3a2c23a0ccd5a78c17
Maxi Ordoñez <exemaxi@outlook.com>
2025-08-27 18:12:56 -0300

    Se corrigui un error en repositorio.

--- src/Components/NavbarPelis.jsx
+++ src/Components/NavbarPelis.jsx
@@ -11,7 +11,7 @@ const NavbarPelis = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Button href="https://github.com/Maxii34/Altas-de-peliculas" target="_blank" variant="dark">Repocitorio</Button>
+            <Button href="https://github.com/Maxii34/Altas-de-peliculas" target="_blank" variant="dark">Repositorio</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
